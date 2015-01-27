4ee6c2a09ba9b728637f05a5468ba69f4694b8d9
David Karlsson <davidkarlsson1@Davids-MacBook-Pro.local>
2015-01-27 14:47:35 +0100

    Project creation

---
+++ RunJournal/RunJournal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunJournal
+//
+//  Created by David Karlsson on 2015-01-27.
+//  Copyright (c) 2015 David Karlsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
